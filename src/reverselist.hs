776b56abfd495af998b4b37c5d2fd1d3d00194f5
Vinny Salvati <vinnysalvati@gmail.com>
2012-06-14 08:08:02 -0400

    update

--- src/reverselist.hs
+++ src/reverselist.hs
@@ -1,11 +1,9 @@
 module Main where
 reverselist:: [Integer] -> [Integer]
-reverselist [] = []
-reverselist (h:t) =  if ( size t == 1) then reverseelements (h:t) else reverseelements(h:reverselist t)
+reverselist [x,y] = [y,x]
+reverselist (h:t) =  if ( size t == 1) then reverselist (h:t) else reverselist(t) ++ [h]
 
 size [] = 0
 size (h:t) = 1 + size t
 
-reverseelements :: [Integer] -> [Integer]
-reverseelements [x,y] = [y,x]
 
